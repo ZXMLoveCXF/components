e229fa0c0d8922f6e0eb62003dcc7e8dd649b20f
zxmlovecxf <352183987@qq.com>
2018-10-23 16:57:53 +0800

    start

---
+++ components/wrap-line/wrap-line.js
@@ -0,0 +1,20 @@
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    line: {
+      type: String,
+      value: '2'
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {},
+  /**
+   * 组件的方法列表
+   */
+  methods: {}
+})
